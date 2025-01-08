411799c9ed5ed759b5a951369f889d8d742394d9
Moiz Qureshi <m.moiz782008@gmail.com>
2025-01-08 14:15:09 +0500

    commint
    push

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import GeneratePassword from '@/components/password-generator'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <GeneratePassword/>
+    </>
+  )
+}
